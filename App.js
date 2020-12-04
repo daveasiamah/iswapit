958b970cb66415cbece404266fe0e1e320931c0e
daveasiamah <daveasiamah@gmail.com>
2020-12-04 00:34:28 +0000

    installing packages

--- App.js
+++ App.js
@@ -1,15 +1,17 @@
+import "react-native-gesture-handler";
 import React from "react";
 import { StyleSheet, View } from "react-native";
 import WelcomeScreen from "./app/screens/WelcomeScreen";
 import ViewImageScreen from "./app/screens/ViewImageScreen";
 import AppText from "./app/components/AppText";
+import { NavigationContainer } from "@react-navigation/native";
 
 export default function App() {
   return (
     <View style={styles.container}>
       {/* <ViewImageScreen /> */}
-      {/* <AppText style={styles.title}>I Love React Native.</AppText> */}
-      <WelcomeScreen />
+      <AppText style={styles.title}>I Love React Native.</AppText>
+      {/* <WelcomeScreen /> */}
     </View>
   );
 }
--- app/config/colors.js
+++ app/config/colors.js
@@ -1,7 +1,9 @@
 export default {
   primary: "#fc5c65",
   secondary: "#4ecdc4",
-  black: "#000",
-  white: "#fff",
+  black: "#000000",
+  white: "#ffffff",
   yellow: "#ffe66d",
+  titleGray: "#2c2c2c",
+  subTitleGray: "#4c4c4c",
 };
