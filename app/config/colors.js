1e167f1bd8c49dd2f6a0e63490b2c26b6ab1e5b1
daveasiamah <daveasiamah@gmail.com>
2021-10-22 18:23:20 +0000

    upgrading to expo 40

--- app/config/colors.js
+++ app/config/colors.js
@@ -1,11 +1,9 @@
 export default {
   primary: "#fc5c65",
   secondary: "#4ecdc4",
-  black: "#000000",
-  white: "#ffffff",
+  black: "#000",
+  white: "#fff",
   yellow: "#ffe66d",
-  titleGray: "#2e2e2e",
-  subTitleGray: "#7e7e7e",
-  backgroundGray: "#f8f4f4",
-  danger: "#ff5454",
+  textBody: "#222222",
+  textHeader: "darkgrey",
 };
